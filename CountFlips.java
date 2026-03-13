7f48f790d60e75db5f8c37ad0f651dffaffa3b29
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2026-03-13 14:12:44 -0400

    coinf flips code

--- CountFlips.java
+++ CountFlips.java
@@ -1,4 +1,30 @@
+import java.util.Random;
 public class CountFlips {
   public static void main(String[] args) {
+    final int HEADS = 0;
+    int face;
+
+     public Coin(){
+      Random random = new Random();
+      flip();
+    }
+
+    public void flip() {
+      face = (int) (Math.random() * 2);
+    }
+     public boolean isHEADS() {
+      RETURN(FACE == HEADS);
+     }
+
+     public String toString(){
+      String faceName;
+
+      if(face == HEADS){
+        faceName = "Heads";
+      }else{
+        faceName = "Tails";
+      }
+      return faceName;
+    }
   }
 }
